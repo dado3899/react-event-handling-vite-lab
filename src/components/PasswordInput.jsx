4a5ce9628522e8ee6e02924848454ab9cf326a63
David Doan <dado3899@colorado.edu>
2025-01-30 15:07:05 -0700

    Update PasswordInput.jsx

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,13 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
-    return(
-        <></>
-    )
+    function handleChange() {
+        console.log("Entering password...")
+      }
+    
+      return (
+        <input type="password" onChange={handleChange} />
+      )
 }
 
 export default PasswordInput;
